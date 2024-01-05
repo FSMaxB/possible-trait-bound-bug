1c4019511231d4c421111b3e9b0603487a36860b
Max Bruckner <max@lumeo.com>
2024-01-05 13:46:14 +0100

    Remove key parameter (not generic parameter though)

--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,21 @@ pub struct RequestBuilder {
 }
 
 impl RequestBuilder {
-    pub fn header<KEY>(mut self, key: KEY) -> Self
+    pub fn header<KEY>(mut self) -> Self
     where
         HeaderName: TryFrom<KEY>,
         <HeaderName as TryFrom<KEY>>::Error: Into<Box<dyn Error>>,
     {
-        let key = match HeaderName::try_from(key) {
-            Ok(key) => key,
-            Err(_) => {
-                return self;
-            }
-        };
-
-        self.reqwest_builder = self.reqwest_builder.header(key, 1234);
+        self.reqwest_builder = self
+            .reqwest_builder
+            .header(http::header::CONTENT_LENGTH, 1234);
         self
     }
 
     fn test(mut self) -> Self {
-        let key = match HeaderName::try_from(http::header::CONTENT_LENGTH) {
-            Ok(key) => key,
-            Err(_) => {
-                return self;
-            }
-        };
-
-        self.reqwest_builder = self.reqwest_builder.header(key, 1234);
+        self.reqwest_builder = self
+            .reqwest_builder
+            .header(http::header::CONTENT_LENGTH, 1234);
         self
     }
 }
